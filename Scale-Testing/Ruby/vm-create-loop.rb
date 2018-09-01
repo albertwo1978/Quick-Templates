9bdc644b506dbe2d53547ec0e9e6b793459102d2
Claudia del Hierro <claudel@microsoft.com>
2018-08-31 17:49:56 -0700

    Create vm-create-loop.rb

---
+++ Scale-Testing/Ruby/vm-create-loop.rb
@@ -0,0 +1,116 @@
+require 'azure_mgmt_resources'
+require 'azure_mgmt_network'
+require 'azure_mgmt_storage'
+require 'azure_mgmt_compute'
+
+
+RG_LOC = 'eastus2'
+GROUP_NAME = 'melitest'
+VNET_RG = 'melinet'
+VNET_NAME = 'melinet'
+SUBNET_NAME = 'melisub'
+
+
+Storage = Azure::Storage::Profiles::Latest::Mgmt
+Network = Azure::Network::Profiles::Latest::Mgmt
+Compute = Azure::Compute::Profiles::Latest::Mgmt
+Resources = Azure::Resources::Profiles::Latest::Mgmt
+
+StorageModels = Storage::Models
+NetworkModels = Network::Models
+ComputeModels = Compute::Models
+ResourceModels = Resources::Models
+
+  subscription_id = ENV['AZURE_SUBSCRIPTION_ID'] || '11111111-1111-1111-1111-111111111111' # your Azure Subscription Id
+  provider = MsRestAzure::ApplicationTokenProvider.new(
+      ENV['AZURE_TENANT_ID'],
+      ENV['AZURE_CLIENT_ID'],
+      ENV['AZURE_CLIENT_SECRET'])
+  credentials = MsRest::TokenCredentials.new(provider)
+
+  options = {
+      credentials: credentials,
+      subscription_id: subscription_id
+  }
+
+  resource_client = Resources::Client.new(options)
+  network_client = Network::Client.new(options)
+  storage_client = Storage::Client.new(options)
+  compute_client = Compute::Client.new(options)
+
+  def print_item(resource)
+    resource.instance_variables.sort.each do |ivar|
+      str = ivar.to_s.gsub /^@/, ''
+      if resource.respond_to? str.to_sym
+        puts "\t\t#{str}: #{resource.send(str.to_sym)}"
+      end
+    end
+    puts "\n\n"
+  end
+
+  
+  subnet = network_client.subnets.get(VNET_RG, VNET_NAME, SUBNET_NAME)
+
+  count = 0
+  arr = []
+  
+  2.times do |i|
+      arr[i] = Thread.new {
+         Thread.current["mycount"] = count
+         number = count
+         count += 1
+         vm_name= "testvm#{number}"
+  print_item nic = network_client.network_interfaces.create_or_update(
+    GROUP_NAME,
+    "sample-ruby-nic-#{number}",
+    NetworkModels::NetworkInterface.new.tap do |interface|
+        interface.location = RG_LOC
+        interface.ip_configurations = [
+            NetworkModels::NetworkInterfaceIPConfiguration.new.tap do |nic_conf|
+                nic_conf.name = "nic-#{number}"
+                nic_conf.private_ipallocation_method = NetworkModels::IPAllocationMethod::Dynamic
+                nic_conf.subnet = subnet
+            end
+        ]
+    end
+)
+vm_create_params = ComputeModels::VirtualMachine.new.tap do |vm|
+    vm.location = RG_LOC
+    vm.os_profile = ComputeModels::OSProfile.new.tap do |os_profile|
+        os_profile.computer_name = vm_name
+        os_profile.admin_username = 'notAdmin'
+        os_profile.admin_password = 'Pa$$w0rd92'
+    end
+    vm.storage_profile = ComputeModels::StorageProfile.new.tap do |store_profile|
+        store_profile.image_reference = ComputeModels::ImageReference.new.tap do |ref|
+            ref.publisher = 'canonical'
+            ref.offer = 'UbuntuServer'
+            ref.sku = '16.04.0-LTS'
+            ref.version = 'latest'
+        end
+        store_profile.os_disk = ComputeModels::OSDisk.new.tap do |os_disk|
+            os_disk.name = "sample-os-disk-#{vm_name}"
+            os_disk.caching = ComputeModels::CachingTypes::None
+            os_disk.create_option = ComputeModels::DiskCreateOptionTypes::FromImage
+        end
+    end
+    vm.hardware_profile = ComputeModels::HardwareProfile.new.tap do |hardware|
+        hardware.vm_size = ComputeModels::VirtualMachineSizeTypes::StandardDS2V2
+    end
+    vm.network_profile = ComputeModels::NetworkProfile.new.tap do |net_profile|
+        net_profile.network_interfaces = [
+            ComputeModels::NetworkInterfaceReference.new.tap do |ref|
+                ref.id = nic.id
+                ref.primary = true
+            end
+        ]
+    end
+end
+
+print_item vm = compute_client.virtual_machines.create_or_update(GROUP_NAME, "sample-ruby-vm-#{vm_name}", vm_create_params)
+    vm
+
+    }
+end
+arr.each {|t| t.join; print t["mycount"], ", " }
+puts "count = #{count}"
